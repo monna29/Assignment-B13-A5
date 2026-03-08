df50a66e30a3effcd90075f43a79979a4b6ac032
monna29 <monnamia255@gmail.com>
2026-03-08 11:26:51 +0600

    add function

--- script/script.js
+++ script/script.js
@@ -35,6 +35,51 @@ function displayIssues(issues) {
     issueCount.innerText = issues.length + " Issues"
 
     issues.forEach(issue => {
+ 
+        function getPriorityClass(priority){
+
+if(priority === "high"){
+return "bg-red-100 text-red-600"
+}
+
+if(priority === "medium"){
+return "bg-yellow-100 text-yellow-700"
+}
+
+return "bg-green-100 text-green-700"
+
+}
+
+
+function getStatusClass(status){
+
+if(status === "open"){
+return "bg-green-100 text-green-700"
+}
+
+return "bg-purple-100 text-purple-700"
+
+}
+
+
+function getLabelClass(label){
+
+if(label === "bug"){
+return "bg-red-100 text-red-600"
+}
+
+if(label === "feature"){
+return "bg-blue-100 text-blue-600"
+}
+
+if(label === "enhancement"){
+return "bg-green-100 text-green-600"
+}
+
+return "bg-gray-100 text-gray-600"
+
+}
+
 
         const border =
             issue.status === "open"
@@ -48,23 +93,47 @@ function displayIssues(issues) {
 
         card.innerHTML = `
 
-<h2 class="font-bold text-lg">
-${issue.title}
-</h2>
+           <div class="flex justify-between">
 
-<p class="text-sm text-gray-600 mt-2">
-${issue.description.slice(0, 80)}...
-</p>
+        ${issue.status === "open"
+                ? "<img src='assets/Open-Status.png' class='w-6'>"
+                : "<img src='assets/Closed- Status .png'>"
+            }
+        
+        <div>
+        <span class="px-2 py-1 text-xs rounded-full ${getPriorityClass(issue.priority)}">
+        ${issue.priority}
+        </span>
+        </div>
+        </div>
 
-<div class="text-xs text-gray-500 mt-3 space-y-1">
-
-<p>Status: ${issue.status}</p>
-<p>Author: ${issue.author}</p>
-<p>Priority: ${issue.priority}</p>
-<p>Label: ${issue.label}</p>
-<p>Created: ${issue.createdAt}</p>
-
-</div>
+        <h2 class="font-bold text-lg">
+        ${issue.title}
+        </h2>
+        
+        <p class="text-sm text-gray-600 mt-2">
+        ${issue.description.slice(0, 80)}...
+        </p>
+        
+        <div class="flex gap-2 mt-3 flex-wrap">
+        
+         <p class="${issue.labels?.[0] === "bug"
+                ? "bg-[#FECACA] text-red-500"
+                : "bg-green-200 text-green-700"
+            } px-2 py-1 rounded inline-block">
+        ${issue.labels?.[0]}
+        </p>  
+        </div>
+   
+        </div>
+        <hr>
+        <p class="text-xs text-gray-500 mt-2">
+        Author: ${issue.author}
+        </p>
+        
+        <p class="text-xs text-gray-400">
+        Created: ${issue.createdAt}
+        </p>
 
 `
 
